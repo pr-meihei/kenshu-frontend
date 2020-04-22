1b73e97913b6bc5c28b8f0050f54792548c653d6
pr-meihei <yohei.ema@prtimes.co.jp>
2020-04-22 15:57:42 +0900

    CSSをScssに書き換え

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps')
 
 gulp.task('sass', function() {
     return gulp.src('./src/sass/**/*.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass({ outputStyle: 'expanded' }))
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('./src/css'));
 });
 
